a5bd325a0013bbb0e9faf4a4c133cd1930a60274
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-03-31 16:49:49 +0200

    Fix user_info for the latest changes

--- app/users/service.py
+++ app/users/service.py
@@ -10,7 +10,8 @@ class UserService:
     def get_current_user_group(db, x_pomerium_jwt_assertion):
         try:
             user_info = UserInfo.get_current(x_pomerium_jwt_assertion)
-            user_data = crud.get_or_create_user_data(db, user_info["sub"])
+            user_id = f"google/{user_info[\"sub\"]}"
+            user_data = crud.get_or_create_user_data(db, user_id)
             return user_data.group
         except Exception as err:
             logger.error(f"User info could not be retrieved: {str(err)}")
